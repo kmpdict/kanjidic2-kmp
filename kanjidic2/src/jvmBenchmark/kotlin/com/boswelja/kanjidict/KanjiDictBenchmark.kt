28eb5edcd4ba3886de12d8eca36d4858b5831ff1
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-08-09 11:39:02 +1000

    Update dependencies and fix compile

--- kanjidic2/src/jvmBenchmark/kotlin/com/boswelja/kanjidict/KanjiDictBenchmark.kt
+++ kanjidic2/src/jvmBenchmark/kotlin/com/boswelja/kanjidict/KanjiDictBenchmark.kt
@@ -1,12 +1,14 @@
 package com.boswelja.kanjidict
 
-import io.github.boswelja.jmdict.kanjidic2.generated.resources.Res
+import com.squareup.zstd.okio.zstdDecompress
+import io.github.boswelja.kanjidic2.kanjidic2.generated.resources.Res
 import kotlinx.benchmark.Benchmark
 import kotlinx.benchmark.Scope
 import kotlinx.benchmark.Setup
 import kotlinx.benchmark.State
 import kotlinx.coroutines.runBlocking
-import java.util.zip.GZIPInputStream
+import okio.Buffer
+import okio.buffer
 
 @State(Scope.Benchmark)
 open class KanjiDictBenchmark {
@@ -16,9 +18,13 @@ open class KanjiDictBenchmark {
     fun prepare() {
         // Lets load the entire file into memory - we want to test the conversion and not IO performance here
         val compressedBytes = runBlocking { Res.readBytes("files/kanjidict.xml") }
-        val reader = GZIPInputStream(compressedBytes.inputStream()).bufferedReader()
-        jmDictSequence = reader.readLines().asSequence()
-        reader.close()
+        val buffer = Buffer()
+        buffer.write(compressedBytes)
+        jmDictSequence = buffer
+            .zstdDecompress()
+            .buffer()
+            .readLines()
+        buffer.close()
     }
 
     @Benchmark
